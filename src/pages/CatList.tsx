ab0b28ae176f57ca56592f41352162bffc6084bb
Noe <noelia2000vane@gmail.com>
2025-04-08 19:57:34 -0400

    image view layout

--- src/pages/CatList.tsx
+++ src/pages/CatList.tsx
@@ -1,10 +1,12 @@
-import { JSX, } from "react";
+import { JSX, useEffect, useState } from "react";
 import {
   ImageList,
   ImageListItem,
   ImageListItemBar,
-  
+  CircularProgress,
+  Box,
 } from "@mui/material";
+
 
 
 const CatList = (): JSX.Element => {
@@ -12,12 +14,26 @@ const CatList = (): JSX.Element => {
 
   return (
     <ImageList
-      
+      sx={{ width: "100%", height: "100%", padding: 2, margin: 0 }}
+      cols={5}
+      gap={10}
+    >
       {cats.map((cat) => (
         <ImageListItem key={cat.id}>
-          
+          <img
+            src={cat.url}
+            alt="Gato"
+            loading="lazy"
+            style={{
+              borderRadius: "12px",
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+            }}
+          />
           <ImageListItemBar
-            
+            title={cat.breeds?.[0]?.name || "Gatito"}
+            position="below"
           />
         </ImageListItem>
       ))}
