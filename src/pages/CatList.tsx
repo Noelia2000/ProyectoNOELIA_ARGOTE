dc78e59ebda7011cf202d2c481d68a41c8775c6b
Noe <noelia2000vane@gmail.com>
2025-04-08 19:46:51 -0400

    I structured the CatList

--- src/pages/CatList.tsx
+++ src/pages/CatList.tsx
@@ -1,3 +1,32 @@
+import { JSX, } from "react";
+import {
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  
+} from "@mui/material";
 
+
+const CatList = (): JSX.Element => {
+  
+
+  return (
+    <ImageList
+      
+      {cats.map((cat) => (
+        <ImageListItem key={cat.id}>
+          
+          <ImageListItemBar
+            
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+};
+
+export default CatList;
+
+  
   
   
